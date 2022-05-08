3a6d3cef3b61738150ed7520b3f748c80ac79ef6
M4D4R4X <hossein.heydari.scc@hotmail.com>
2022-05-08 15:30:33 +0100

    minor changes

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -8,7 +8,7 @@ function NewExpense(props) {
             id:Math.random().toString()
         };
         props.onAddExpense(expenseData)
-        console.log(expenseData)
+        
     }
   return (
     <div className="new-expense">
