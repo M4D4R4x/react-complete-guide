fa0642627843a974313c69972c870f197c479741
M4D4R4X <hossein.heydari.scc@hotmail.com>
2022-05-11 13:49:58 +0100

    added dynamic list

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import ExpenseItem from "./ExpenseItem";
+import React,{useState} from 'react'
 import '../../styling/Expenses.css'
 import Card from "../UI/Card";
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 function Expenses(props) {
+  const [selectedYear,setSelectedYear] =useState('2022')
+  const ChangeYearDataHandler = (selectedYearData) => {
+setSelectedYear(selectedYearData);
+  };
+const filteredExpenses = props.items.filter(expense => {
+  return expense.date.getFullYear().toString() === selectedYear;
+});
+
   return (
+    <li>
+      
     <Card className="expenses">
-      <ExpenseItem title={props.items[0].title} amount={props.items[0].amount}  date={props.items[0].date}/>
-      <ExpenseItem title={props.items[1].title} amount={props.items[1].amount}  date={props.items[1].date}/>
-      <ExpenseItem title={props.items[2].title} amount={props.items[2].amount}  date={props.items[2].date}/>
-      <ExpenseItem title={props.items[3].title} amount={props.items[3].amount}  date={props.items[3].date}/>
-    </Card>
-  )
-}
+    <ExpensesFilter selectedYear={selectedYear} onChangeYearData={ChangeYearDataHandler} />
+    <ExpensesList list={filteredExpenses} />
+</Card>
+    </li>
+  );
+};
 
 export default Expenses
