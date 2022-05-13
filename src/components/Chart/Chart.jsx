ce6ec3d59bdc1ec1c384e71e448fc98be8633ce9
M4D4R4X <hossein.heydari.scc@hotmail.com>
2022-05-13 08:30:23 +0100

    started building Chart Component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import '../../styling/Chart.css';
+import ChartBar from './ChartBar'
 
-function Chart(props) {
+function Chart(props) { 
   return (
-    <div>Chart</div>
+    <div className="chart">
+      {props.data.map(chartData =><ChartBar value={chartData.value} maxValue={null} label={chartData.label} key={chartData.label} />)}
+      </div>
   )
 }
 
